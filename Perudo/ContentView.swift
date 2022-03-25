3a60d9c8b26db267d03a13380a180d8377e3359d
Alistair Begg <begg.a@etoncollege.org.uk>
2022-03-25 14:55:26 +0000

    Initial Commit

---
+++ Perudo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Perudo
+//
+//  Created by Begg, Alistair (AMM) on 25/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Perudo/PerudoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PerudoApp.swift
+//  Perudo
+//
+//  Created by Begg, Alistair (AMM) on 25/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct PerudoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
